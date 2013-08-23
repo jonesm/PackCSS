8b728ec1c4bfb7a7830d24836656bfacafadca78
Matthew L Jones <mljphpdev@gmail.com>
2013-08-23 14:28:33 -0700

    Stable

---
+++ PackCSS/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PackCSS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.pnlDragTo = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 144);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(260, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // pnlDragTo
+            // 
+            this.pnlDragTo.AllowDrop = true;
+            this.pnlDragTo.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pnlDragTo.Location = new System.Drawing.Point(12, 33);
+            this.pnlDragTo.Name = "pnlDragTo";
+            this.pnlDragTo.Size = new System.Drawing.Size(260, 105);
+            this.pnlDragTo.TabIndex = 2;
+            this.pnlDragTo.DragDrop += new System.Windows.Forms.DragEventHandler(this.pnlDragTo_DragDrop);
+            this.pnlDragTo.DragEnter += new System.Windows.Forms.DragEventHandler(this.pnlDragTo_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Drag and Drop CSS file below.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 175);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnlDragTo);
+            this.Controls.Add(this.progressBar);
+            this.Name = "Form1";
+            this.Text = "PackCSS v1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Panel pnlDragTo;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PackCSS/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PackCSS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pack(string[] files)
+        {
+
+            foreach (string file in files)
+            {
+                
+                // Begin
+                progressBar.Value = 0;
+
+                // Read dropped file
+                string css = System.IO.File.ReadAllText(file);
+
+                // Get CSS file info
+                FileInfo cssInfo = new FileInfo(file);
+                // Get file's directory
+                string directory = cssInfo.DirectoryName + "\\";
+                
+                // Get file's name and remove extension
+                string name = cssInfo.Name;
+                name = name.Substring(0, name.LastIndexOf("."));
+                System.Console.WriteLine(name);
+
+                // Set positions to beginning of file
+                Int32 positionBegin = 0;
+                Int32 positionEnd = 0;
+                Int32 positionLast = 0;
+
+                do
+                {
+
+                    // This is used to prevent process of unknown file types
+                    bool unknown = false;
+
+                    // Find url(
+                    positionBegin = css.IndexOf("url(", positionBegin) + 4; // 4 to skip url(
+
+                    // Find )
+                    positionEnd = css.IndexOf(")", positionBegin);
+
+                    // Check if we found something to process
+                    if (positionBegin == -1 || positionEnd == -1
+                        || positionLast > positionEnd) break;
+
+                    positionLast = positionEnd;
+
+                    // Create path to resource file
+                    string url = css.Substring(positionBegin, positionEnd - positionBegin);
+                    string path = (directory + url).Replace("/", "\\");
+                    
+                    // Check if the path is base64 data
+                    if (path.Contains("data:")) continue;
+
+                    //System.Console.WriteLine(url);
+                    
+                    // Read url file
+                    byte[] contents = System.IO.File.ReadAllBytes(path);
+                    
+                    // Base64 the file data
+                    //var bytes = Encoding.UTF8.GetBytes(contents);
+                    var base64 = Convert.ToBase64String(contents);
+
+                    // Get file information
+                    FileInfo fileInfo = new FileInfo(path);
+
+                    // Update bar
+                    progressBar.Value = (int)((float)positionEnd / (float)fileInfo.Length) % 100;
+
+                    // Get extension of file
+                    string extension = fileInfo.Extension; // will include .
+                    
+                    // Prepend data:type/format;base64,
+                    switch (fileInfo.Extension)
+                    {
+                        // CSS is only worried about images and fonts
+                        // Images
+                        case ".gif":
+                            base64 = "data:image/gif;base64," + base64;
+                            break;
+                        case ".jpeg":
+                        case ".jpg":
+                            base64 = "data:image/jpeg;base64," + base64;
+                            break;
+                        case ".pjpeg":
+                            base64 = "data:image/pjpeg;base64," + base64;
+                            break;
+                        case ".png":
+                            base64 = "data:image/png;base64," + base64;
+                            break;
+                        case ".tiff":
+                        case ".tif":
+                            base64 = "data:image/tiff;base64," + base64;
+                            break;
+                        case ".svg": // Can be a font too
+                            base64 = "data:image/svg+xml;base64," + base64;
+                            break;
+                        // Fonts
+                        case ".ttf":
+                            base64 = "data:application/x-font-ttf;base64," + base64;
+                            break;
+                        case ".otf":
+                            base64 = "data:application/x-font-opentype;base64," + base64;
+                            break;
+                        case ".woff":
+                            base64 = "data:application/font-woff;base64," + base64;
+                            break;
+                        case ".eot":
+                            base64 = "data:application/vnd.ms-fontobject;base64," + base64;
+                            break;
+                        default:
+                            unknown = true;
+                            break;
+                    };
+
+                    // Replace url with base64 data
+                    if (!unknown) css = css.Replace(url, base64);
+
+                } while (true);
+
+                // Write file output
+                System.IO.File.WriteAllText(directory + name + "-packed.css", css);
+
+                // Finished
+                progressBar.Value = 100;
+
+            }
+        }
+
+        private void pnlDragTo_DragEnter(object sender, DragEventArgs e)
+        {
+            //if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Move;
+            //else
+              //  e.Effect = DragDropEffects.None;
+        }
+
+        private void pnlDragTo_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                pack(files);
+            }
+        }
+
+    }
+}
